8306994af252bd2bd491a22b3ae355265982ea56
javed-tech <jk2169910@gmail.com>
2022-08-07 19:06:34 +0530

    Updates

---
+++ precache-manifest.d26a726f4d391d0c2c81103e8d46f25d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8b6b444ad61a44edb6373a36cfeea6",
+    "url": "/TODO-List-Project/index.html"
+  },
+  {
+    "revision": "d205e66eca1b6a4633bb",
+    "url": "/TODO-List-Project/static/css/2.8b22ea70.chunk.css"
+  },
+  {
+    "revision": "5192ce24ff1ee6c2d4bf",
+    "url": "/TODO-List-Project/static/css/main.686842f3.chunk.css"
+  },
+  {
+    "revision": "d205e66eca1b6a4633bb",
+    "url": "/TODO-List-Project/static/js/2.89270ee5.chunk.js"
+  },
+  {
+    "revision": "45655d2f064e3048f316421d96d8becf",
+    "url": "/TODO-List-Project/static/js/2.89270ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5192ce24ff1ee6c2d4bf",
+    "url": "/TODO-List-Project/static/js/main.1496571f.chunk.js"
+  },
+  {
+    "revision": "c97d664276da0735fd4d",
+    "url": "/TODO-List-Project/static/js/runtime-main.afa9dd94.js"
+  }
+]);
